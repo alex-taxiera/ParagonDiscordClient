c99214e59aba303465b43211553f714f4b1640d5
alex-taxiera <alex.taxiera@gmail.com>
2018-07-18 15:07:28 -0400

    mess with size and spacing

--- ParagonDiscordClient/PlayParagonForm.Designer.cs
+++ ParagonDiscordClient/PlayParagonForm.Designer.cs
@@ -31,6 +31,7 @@ namespace ParagonDiscordClient
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayParagonForm));
       this.heroLabel = new System.Windows.Forms.Label();
       this.mapLabel = new System.Windows.Forms.Label();
       this.Play = new System.Windows.Forms.Button();
@@ -40,8 +41,9 @@ namespace ParagonDiscordClient
       // 
       // heroLabel
       // 
+      this.heroLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
       this.heroLabel.AutoSize = true;
-      this.heroLabel.Location = new System.Drawing.Point(12, 32);
+      this.heroLabel.Location = new System.Drawing.Point(8, 16);
       this.heroLabel.Name = "heroLabel";
       this.heroLabel.Size = new System.Drawing.Size(69, 13);
       this.heroLabel.TabIndex = 2;
@@ -49,8 +51,9 @@ namespace ParagonDiscordClient
       // 
       // mapLabel
       // 
+      this.mapLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
       this.mapLabel.AutoSize = true;
-      this.mapLabel.Location = new System.Drawing.Point(12, 75);
+      this.mapLabel.Location = new System.Drawing.Point(8, 48);
       this.mapLabel.Name = "mapLabel";
       this.mapLabel.Size = new System.Drawing.Size(67, 13);
       this.mapLabel.TabIndex = 3;
@@ -58,7 +61,8 @@ namespace ParagonDiscordClient
       // 
       // Play
       // 
-      this.Play.Location = new System.Drawing.Point(51, 117);
+      this.Play.Anchor = System.Windows.Forms.AnchorStyles.None;
+      this.Play.Location = new System.Drawing.Point(56, 88);
       this.Play.Name = "Play";
       this.Play.Size = new System.Drawing.Size(136, 23);
       this.Play.TabIndex = 4;
@@ -68,8 +72,9 @@ namespace ParagonDiscordClient
       // 
       // heroBox
       // 
+      this.heroBox.Anchor = System.Windows.Forms.AnchorStyles.None;
       this.heroBox.FormattingEnabled = true;
-      this.heroBox.Location = new System.Drawing.Point(99, 29);
+      this.heroBox.Location = new System.Drawing.Point(112, 16);
       this.heroBox.Name = "heroBox";
       this.heroBox.Size = new System.Drawing.Size(121, 21);
       this.heroBox.Sorted = true;
@@ -77,8 +82,9 @@ namespace ParagonDiscordClient
       // 
       // mapBox
       // 
+      this.mapBox.Anchor = System.Windows.Forms.AnchorStyles.None;
       this.mapBox.FormattingEnabled = true;
-      this.mapBox.Location = new System.Drawing.Point(99, 72);
+      this.mapBox.Location = new System.Drawing.Point(112, 48);
       this.mapBox.Name = "mapBox";
       this.mapBox.Size = new System.Drawing.Size(121, 21);
       this.mapBox.TabIndex = 1;
@@ -87,12 +93,15 @@ namespace ParagonDiscordClient
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(236, 163);
+      this.ClientSize = new System.Drawing.Size(244, 121);
       this.Controls.Add(this.Play);
       this.Controls.Add(this.mapLabel);
       this.Controls.Add(this.heroLabel);
       this.Controls.Add(this.mapBox);
       this.Controls.Add(this.heroBox);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.MaximumSize = new System.Drawing.Size(260, 160);
+      this.MinimumSize = new System.Drawing.Size(260, 160);
       this.Name = "PlayParagonForm";
       this.Text = "Play Paragon";
       this.ResumeLayout(false);
