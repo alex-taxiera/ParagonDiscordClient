bc0abb035982c692063542f3a35bdf81a417aaef
alex-taxiera <alex.taxiera@gmail.com>
2018-07-18 12:23:43 -0400

    remove static form asset map from backend

--- ParagonDiscordClient/ParagonPresence.cs
+++ ParagonDiscordClient/ParagonPresence.cs
@@ -25,54 +25,6 @@ namespace ParagonDiscordClient
       SmallImageKey = "epic",
       SmallImageText = "Epic Games"
     };
-    private static List<KeyValuePair<string, string>> smallAssets = new List<KeyValuePair<string, string>>
-    {
-      new KeyValuePair<string, string>("aurora", "Aurora"),
-      new KeyValuePair<string, string>("belica", "Lt. Belica"),
-      new KeyValuePair<string, string>("countess", "Countess"),
-      new KeyValuePair<string, string>("crunch", "Crunch"),
-      new KeyValuePair<string, string>("dekker", "Dekker"),
-      new KeyValuePair<string, string>("drongo", "Drongo"),
-      new KeyValuePair<string, string>("feng_mao", "Feng Mao"),
-      new KeyValuePair<string, string>("fey", "The Fey"),
-      new KeyValuePair<string, string>("gadget", "Gadget"),
-      new KeyValuePair<string, string>("gideon", "Gideon"),
-      new KeyValuePair<string, string>("greystone", "Greystone"),
-      new KeyValuePair<string, string>("grim_exe", "Grim.exe"),
-      new KeyValuePair<string, string>("grux", "Grux"),
-      new KeyValuePair<string, string>("howitzer", "Howitzer"),
-      new KeyValuePair<string, string>("iggy", "Iggy & Scorch"),
-      new KeyValuePair<string, string>("kallari", "Kallari"),
-      new KeyValuePair<string, string>("khaimera", "Khaimera"),
-      new KeyValuePair<string, string>("kwang", "Kwang"),
-      new KeyValuePair<string, string>("morigesh", "Morigesh"),
-      new KeyValuePair<string, string>("murdock", "Murdock"),
-      new KeyValuePair<string, string>("muriel", "Muriel"),
-      new KeyValuePair<string, string>("narbash", "Narbash"),
-      new KeyValuePair<string, string>("phase", "Phase"),
-      new KeyValuePair<string, string>("rampage", "Rampage"),
-      new KeyValuePair<string, string>("revenant", "Revenant"),
-      new KeyValuePair<string, string>("riktor", "Riktor"),
-      new KeyValuePair<string, string>("serath", "Serath"),
-      new KeyValuePair<string, string>("sevarog", "Sevarog"),
-      new KeyValuePair<string, string>("shinbi", "Shinbi"),
-      new KeyValuePair<string, string>("sparrow", "Sparrow"),
-      new KeyValuePair<string, string>("steel", "Steel"),
-      new KeyValuePair<string, string>("terra", "Terra"),
-      new KeyValuePair<string, string>("twinblast", "Twinblast"),
-      new KeyValuePair<string, string>("wraith", "Wraith"),
-      new KeyValuePair<string, string>("wukong", "Wukong"),
-      new KeyValuePair<string, string>("yin", "Yin"),
-      new KeyValuePair<string, string>("zinx", "Zinx")
-    };
-    private static List<KeyValuePair<string, string>> largeAssets = new List<KeyValuePair<string, string>>
-    {
-      new KeyValuePair<string, string>("legacy_jungle", "Legacy"),
-      new KeyValuePair<string, string>("monolith", "Monolith")
-    };
-
-    public List<KeyValuePair<string, string>> LargeAssets { get => largeAssets; set => largeAssets = value; }
-    public List<KeyValuePair<string, string>> SmallAssets { get => smallAssets; set => smallAssets = value; }
 
     public ParagonPresence()
     {
