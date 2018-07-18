2717fbcc4548edd6ff3c7f91c9e4365f6b3a2d23
alex-taxiera <alex.taxiera@gmail.com>
2018-07-18 12:21:06 -0400

    add windows form

---
+++ ParagonDiscordClient/PlayParagonForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ParagonDiscordClient
+{
+  partial class PlayParagonForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.heroLabel = new System.Windows.Forms.Label();
+      this.mapLabel = new System.Windows.Forms.Label();
+      this.Play = new System.Windows.Forms.Button();
+      this.heroBox = new System.Windows.Forms.ComboBox();
+      this.mapBox = new System.Windows.Forms.ComboBox();
+      this.SuspendLayout();
+      // 
+      // heroLabel
+      // 
+      this.heroLabel.AutoSize = true;
+      this.heroLabel.Location = new System.Drawing.Point(12, 32);
+      this.heroLabel.Name = "heroLabel";
+      this.heroLabel.Size = new System.Drawing.Size(69, 13);
+      this.heroLabel.TabIndex = 2;
+      this.heroLabel.Text = "Choose Hero";
+      // 
+      // mapLabel
+      // 
+      this.mapLabel.AutoSize = true;
+      this.mapLabel.Location = new System.Drawing.Point(12, 75);
+      this.mapLabel.Name = "mapLabel";
+      this.mapLabel.Size = new System.Drawing.Size(67, 13);
+      this.mapLabel.TabIndex = 3;
+      this.mapLabel.Text = "Choose Map";
+      // 
+      // Play
+      // 
+      this.Play.Location = new System.Drawing.Point(51, 117);
+      this.Play.Name = "Play";
+      this.Play.Size = new System.Drawing.Size(136, 23);
+      this.Play.TabIndex = 4;
+      this.Play.Text = "Matchmake and Play!";
+      this.Play.UseVisualStyleBackColor = true;
+      this.Play.Click += new System.EventHandler(this.Play_Click);
+      // 
+      // heroBox
+      // 
+      this.heroBox.FormattingEnabled = true;
+      this.heroBox.Location = new System.Drawing.Point(99, 29);
+      this.heroBox.Name = "heroBox";
+      this.heroBox.Size = new System.Drawing.Size(121, 21);
+      this.heroBox.Sorted = true;
+      this.heroBox.TabIndex = 0;
+      // 
+      // mapBox
+      // 
+      this.mapBox.FormattingEnabled = true;
+      this.mapBox.Location = new System.Drawing.Point(99, 72);
+      this.mapBox.Name = "mapBox";
+      this.mapBox.Size = new System.Drawing.Size(121, 21);
+      this.mapBox.TabIndex = 1;
+      // 
+      // PlayParagonForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(236, 163);
+      this.Controls.Add(this.Play);
+      this.Controls.Add(this.mapLabel);
+      this.Controls.Add(this.heroLabel);
+      this.Controls.Add(this.mapBox);
+      this.Controls.Add(this.heroBox);
+      this.Name = "PlayParagonForm";
+      this.Text = "Play Paragon";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private Label heroLabel;
+    private Label mapLabel;
+    private Button Play;
+    private ComboBox heroBox;
+    private ComboBox mapBox;
+  }
+}
+
---
+++ ParagonDiscordClient/PlayParagonForm.cs
@@ -0,0 +1,78 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParagonDiscordClient
+{
+  public partial class PlayParagonForm : Form
+  {
+    ParagonPresence presence = new ParagonPresence();
+    private static List<KeyValuePair<string, string>> smallAssets = new List<KeyValuePair<string, string>>
+    {
+      new KeyValuePair<string, string>("aurora", "Aurora"),
+      new KeyValuePair<string, string>("belica", "Lt. Belica"),
+      new KeyValuePair<string, string>("countess", "Countess"),
+      new KeyValuePair<string, string>("crunch", "Crunch"),
+      new KeyValuePair<string, string>("dekker", "Dekker"),
+      new KeyValuePair<string, string>("drongo", "Drongo"),
+      new KeyValuePair<string, string>("feng_mao", "Feng Mao"),
+      new KeyValuePair<string, string>("fey", "The Fey"),
+      new KeyValuePair<string, string>("gadget", "Gadget"),
+      new KeyValuePair<string, string>("gideon", "Gideon"),
+      new KeyValuePair<string, string>("greystone", "Greystone"),
+      new KeyValuePair<string, string>("grim_exe", "Grim.exe"),
+      new KeyValuePair<string, string>("grux", "Grux"),
+      new KeyValuePair<string, string>("howitzer", "Howitzer"),
+      new KeyValuePair<string, string>("iggy", "Iggy & Scorch"),
+      new KeyValuePair<string, string>("kallari", "Kallari"),
+      new KeyValuePair<string, string>("khaimera", "Khaimera"),
+      new KeyValuePair<string, string>("kwang", "Kwang"),
+      new KeyValuePair<string, string>("morigesh", "Morigesh"),
+      new KeyValuePair<string, string>("murdock", "Murdock"),
+      new KeyValuePair<string, string>("muriel", "Muriel"),
+      new KeyValuePair<string, string>("narbash", "Narbash"),
+      new KeyValuePair<string, string>("phase", "Phase"),
+      new KeyValuePair<string, string>("rampage", "Rampage"),
+      new KeyValuePair<string, string>("revenant", "Revenant"),
+      new KeyValuePair<string, string>("riktor", "Riktor"),
+      new KeyValuePair<string, string>("serath", "Serath"),
+      new KeyValuePair<string, string>("sevarog", "Sevarog"),
+      new KeyValuePair<string, string>("shinbi", "Shinbi"),
+      new KeyValuePair<string, string>("sparrow", "Sparrow"),
+      new KeyValuePair<string, string>("steel", "Steel"),
+      new KeyValuePair<string, string>("terra", "Terra"),
+      new KeyValuePair<string, string>("twinblast", "Twinblast"),
+      new KeyValuePair<string, string>("wraith", "Wraith"),
+      new KeyValuePair<string, string>("wukong", "Wukong"),
+      new KeyValuePair<string, string>("yin", "Yin"),
+      new KeyValuePair<string, string>("zinx", "Zinx")
+    };
+    private static List<KeyValuePair<string, string>> largeAssets = new List<KeyValuePair<string, string>>
+    {
+      new KeyValuePair<string, string>("legacy_jungle", "Legacy"),
+      new KeyValuePair<string, string>("monolith", "Monolith")
+    };
+    public PlayParagonForm()
+    {
+      InitializeComponent();
+      mapBox.DataSource = new BindingSource(largeAssets, null);
+      mapBox.DisplayMember = "Value";
+      mapBox.ValueMember = "Key";
+      heroBox.DataSource = new BindingSource(smallAssets, null);
+      heroBox.DisplayMember = "Value";
+      heroBox.ValueMember = "Key";
+    }
+
+    private async void Play_Click(object sender, EventArgs e)
+    {
+      KeyValuePair<string, string> selectedHero = (KeyValuePair<string, string>)heroBox.SelectedItem;
+      KeyValuePair<string, string> selectedMap = (KeyValuePair<string, string>)mapBox.SelectedItem;
+      Play.Enabled = false;
+      await presence.MatchmakeAndPlayAsync(selectedHero, selectedMap);
+      Play.Enabled = true;
+    }
+  }
+}
